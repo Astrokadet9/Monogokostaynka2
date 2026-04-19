4fb7259b768aad6add96461b74f21e87e72edf73
Mnogokostyanka <algoastro219@gmail.com>
2026-04-19 18:01:31 +0500

    commit

---
+++ cmd/habr.parser.py
@@ -0,0 +1,32 @@
+import requests 
+from bs4 import BeautifulSoup
+from utils import check_and_create_result
+
+URL_1 = 'https://www.wikipedia.org/'
+URL_2 = 'https://www.python.org/'
+
+def main():
+    req = requests.get(URL_1, headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36'
+    })
+    if req.status_code == 200:
+        print(req.headers)
+        soup = BeautifulSoup(req.content, 'html.parser')
+
+        lang_list = soup.find_all('a', class_='link-box')
+
+        results_path check_and_create_result()
+            
+        with open('../result/wikipedia-results.txt', 'w', encoding='utf-8') as f:
+            for element in lang_list:
+                url = f"https:{element['href']}"
+                lang = element.find('strong').text
+                cnt = element.find('small').text
+                print(lang, url, cnt)
+                f.write(f'{url};{lang};{cnt}\n')
+
+    else:
+        print(f'Ошибка запроса\nStatuse Code: {req.status_code}')
+
+if __name__ == '__main__':
+    main()
